57a99570252b8b6792b971ba95fe9d89059f1c7b
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-02 22:00:33 -0400

    Update variables.tf

--- cloud_exclusive_online/variables.tf
+++ cloud_exclusive_online/variables.tf
@@ -1,63 +1,74 @@
-variable "location" {
-  type        = string
-  description = "Azure region where resources will be deployed"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
-}
-
-variable "route_table_name" {
-  type        = string
-  description = "Name of the route table"
-}
+variable "location" {}
+variable "resource_group_name" {}
+variable "route_table_name" {}
+variable "vnet_name" {}
+variable "subnet_name" {}
+variable "subnet_address_prefixes" {}
+variable "address_space_vnet1" {}
+variable "enable_vm_protection" {}
+variable "dns_servers" {}
+variable "storage_account_id" {}
+variable "log_analytics_workspace_id" {}
 
 variable "nsg_name" {
+  description = "The name of the Network Security Group"
   type        = string
-  description = "Name of the network security group"
 }
 
-variable "vnet_name" {
+variable "storage_account_id" {
+  description = "The ID of the storage account"
   type        = string
-  description = "Name of the virtual network"
+  default     = "<default-storage-account-id>" # Replace if needed
 }
 
-variable "address_space_vnet1" {
-  type        = list(string)
-  description = "Address space for the virtual network"
-}
-
-variable "dns_servers" {
-  type        = list(string)
-  description = "List of DNS servers"
-}
-
-variable "enable_vm_protection" {
-  type        = bool
-  description = "Enable VM protection"
-}
-
-variable "encryption" {
-  type        = bool
-  description = "Enable encryption"
-}
-
-variable "subnet_name" {
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics Workspace"
   type        = string
-  description = "Subnet name"
+  default     = "<default-log-analytics-id>" # Replace if needed
 }
 
-variable "subnet_address_prefixes" {
-  type        = list(string)
-  description = "Subnet address prefixes"
+# Parameterizing Route Table `next_hop_type`
+variable "next_hop_type" {
+  description = "Type of next hop (e.g., VirtualAppliance, Internet, VnetLocal)"
+  type        = string
+  default     = "VirtualAppliance"
 }
 
+# Parameterizing Routes
 variable "routes" {
+  description = "List of routes"
   type = list(object({
     name           = string
     address_prefix = string
-    next_hop_ip    = string
+    next_hop_ip    = optional(string)
   }))
-  description = "List of routes for the route table"
+}
+
+# Parameterizing Security Rules
+variable "security_rules" {
+  description = "List of security rules for the NSG"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_address_prefix      = string
+    source_port_range          = string
+    destination_address_prefix = string
+    destination_port_range     = string
+  }))
+  default = [
+    {
+      name                       = "DenyInboundHTTPS"
+      priority                   = 4096
+      direction                  = "Inbound"
+      access                     = "Deny"
+      protocol                   = "*"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+      destination_address_prefix = "*"
+      destination_port_range     = "*"
+    }
+  ]
 }
