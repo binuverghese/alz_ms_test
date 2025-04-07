a679817dc4f941d39226cb5d47d3861dcf12e640
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-07 12:53:29 -0400

    Create variables.tf

---
+++ online/variables.tf
@@ -0,0 +1,42 @@
+variable "application_gateway_name" {
+  description = "Name of the Application Gateway"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "appgw_subnet_id" {
+  description = "Subnet ID for the Application Gateway"
+  type        = string
+}
+
+variable "appgw_public_ip_id" {
+  description = "Public IP ID for the Application Gateway"
+  type        = string
+}
+
+variable "sku_name" {
+  description = "SKU name for App Gateway"
+  type        = string
+  default     = "Standard_v2"
+}
+
+variable "sku_tier" {
+  description = "SKU tier for App Gateway"
+  type        = string
+  default     = "Standard_v2"
+}
+
+variable "capacity" {
+  description = "Instance count (capacity)"
+  type        = number
+  default     = 2
+}
