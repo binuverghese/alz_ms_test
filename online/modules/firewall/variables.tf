6cc9363afd2141a8d2d0f96e638bfbb2e9de7d4d
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-07 15:28:56 -0400

    Create variables.tf

---
+++ online/modules/firewall/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "firewall_sku_name" {
+  type = string
+}
+
+variable "firewall_sku_tier" {
+  type = string
+}
+
+variable "firewall_policy_id" {
+  type = string
+}
+
+variable "firewall_ipconfig_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "public_ip_address_id" {
+  type = string
+}
