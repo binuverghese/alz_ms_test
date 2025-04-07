8dc496d528c826afe002d6a6d7f482794a46c0c4
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-07 15:27:09 -0400

    Update main.tf

--- online/modules/networking/main.tf
+++ online/modules/networking/main.tf
@@ -1,1 +1,24 @@
+module "hub_vnet" {
+  source              = "Azure/avm-res-network-virtualnetwork/azurerm"
+  name                = "hub-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  enable_telemetry    = false
+
+  subnets = {
+    AzureFirewallSubnet = {
+      name             = "AzureFirewallSubnet"
+      address_prefixes = ["10.0.0.0/24"]
+    }
+    AppGatewaySubnet = {
+      name             = "AppGatewaySubnet"
+      address_prefixes = ["10.0.3.0/24"]
+    }
+  }
+}
+
+output "appgw_subnet_id" {
+  value = module.hub_vnet.subnets["AppGatewaySubnet"].resource_id
+}
 
