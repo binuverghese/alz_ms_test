ef5014f4677824d264b4c5a22ba00a07ebdaa74a
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-07 15:26:01 -0400

    Update variables.tf

--- online/modules/app-gateway/variables.tf
+++ online/modules/app-gateway/variables.tf
@@ -1,1 +1,20 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "public_ip_address_id" {
+  type = string
+}
+
+variable "appgw_subnet_id" {
+  type = string
+}
 
