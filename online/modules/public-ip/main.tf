bf185e1fc1d53d6dba05bf1d990877e827005915
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-07 15:26:47 -0400

    Update main.tf

--- online/modules/public-ip/main.tf
+++ online/modules/public-ip/main.tf
@@ -1,1 +1,12 @@
+resource "azurerm_public_ip" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = [1, 2, 3]
+}
 
+output "id" {
+  value = azurerm_public_ip.this.id
+}
