9c466cfc1acbcb6e3385131583b8d2b5381300a8
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-08 15:35:26 -0400

    Update variables.tf

--- online/variables.tf
+++ online/variables.tf
@@ -1,21 +1,25 @@
 variable "location" {
   description = "The Azure region to deploy resources in."
   type        = string
+  default     = "Canada Central"
 }
 
 variable "main_rg_name" {
   description = "Name of the main resource group."
   type        = string
+  default     = "rg-dev-003"
 }
 
 variable "appgw_rg_name" {
-  description = "Name of the resource group for Application Gateway."
+  description = "Name of the resource group for Application Gateway."  
   type        = string
-}
+  default     = "rg-appgw-dev"
+ }
 
 variable "hub_vnet_address_space" {
   description = "Address space for the Hub VNet."
   type        = list(string)
+  default     = ["10.0.0.0/16"]
 }
 
 variable "hub_subnets" {
@@ -24,11 +28,22 @@ variable "hub_subnets" {
     name             = string
     address_prefixes = list(string)
   }))
+  default = {
+    AzureFirewallSubnet = {
+      name             = "AzureFirewallSubnet"
+      address_prefixes = ["10.0.0.0/24"]
+    }
+    AppGatewaySubnet = {
+      name             = "AppGatewaySubnet"
+      address_prefixes = ["10.0.3.0/24"]
+    }
+  }
 }
 
 variable "appgw_name" {
   description = "Name of the Application Gateway."
   type        = string
+  default     = "appgw-dev-003"
 }
 
 variable "appgw_sku" {
@@ -38,6 +53,11 @@ variable "appgw_sku" {
     tier     = string
     capacity = number
   })
+  default = {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
 }
 
 variable "appgw_backend_addresses" {
@@ -45,22 +65,62 @@ variable "appgw_backend_addresses" {
   type = list(object({
     ip_address = string
   }))
-}
-
-variable "appgw_zones" {
-  description = "Availability Zones for App Gateway Public IP."
-  type        = list(number)
+  default = [
+    { ip_address = "10.1.1.4" },
+    { ip_address = "10.1.1.5" }
+  ]
 }
 
 variable "appgw_public_ip_name" {
   description = "Name for the App Gateway public IP."
   type        = string
+  default     = "appgw-pip"
 }
 
-variable "public_ip_address_id" {
-  type = string
+variable "firewall_name" {
+  default = "fw-dev"
 }
 
-variable "appgw_subnet_id" {
-  type = string
+variable "firewall_public_ip_name" {
+  default = "fw-pip"
+}
+
+variable "firewall_sku_name" {
+  default = "AZFW_VNet"
+}
+
+variable "firewall_sku_tier" {
+  default = "Standard"
+}
+
+variable "firewall_ipconfig_name" {
+  default = "fw-ipconfig"
+}
+
+variable "firewall_policy_id" {
+  default = ""  
+}
+
+variable "bastion_name" {
+  default = "bastion-dev"
+}
+
+variable "bastion_public_ip_name" {
+  default = "bastion-pip"
+}
+
+variable "bastion_ip_config_name" {
+  default = "bastion-ipconfig"
+}
+
+variable "tags" {
+  default = {
+    environment = "dev"
+    owner       = "network-team"
+  }
+}
+
+variable "firewall_policy_name" {
+  type        = string
+  description = "The name of the Azure Firewall Policy"
 }
