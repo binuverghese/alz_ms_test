c115b19ac5948b44a42149288d4f5ef60b2e6d47
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 15:03:21 -0400

    Update main.tf

--- terraform-template/modules/route_table/main.tf
+++ terraform-template/modules/route_table/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_route_table" "this" {
   }
 }
 
-resource "azurerm_subnet_route_table_association" "assoc" {
-  subnet_id      = var.subnet_id
-  route_table_id = azurerm_route_table.this.id
-}
+#resource "azurerm_subnet_route_table_association" "assoc" {
+#  subnet_id      = var.subnet_id
+ # route_table_id = azurerm_route_table.this.id
+#}
