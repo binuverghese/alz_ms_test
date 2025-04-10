d0f1a64393b48df367bdf859fa19012d4fa45d76
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 22:32:21 -0400

    Update main.tf

--- firewall_bastion_hub/modules/bastion/main.tf
+++ firewall_bastion_hub/modules/bastion/main.tf
@@ -1,12 +1,19 @@
-resource "azurerm_bastion_host" "bastion" {
+resource "azurerm_bastion_host" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  dns_name            = var.dns_name
 
   ip_configuration {
-    name                 = "ipconfig1"
-    subnet_id            = var.subnet_id
+    name                 = "configuration"
     public_ip_address_id = var.public_ip_id
+    subnet_id            = var.subnet_id
   }
+
+  dns_name             = var.dns_name
+  scale_units          = var.scale_units
+  ip_connect_enabled   = var.ip_connect_enabled
+  tunneling_enabled    = var.tunneling_enabled
+  kerberos_enabled     = var.kerberos_enabled
+
+  tags = var.tags
 }
