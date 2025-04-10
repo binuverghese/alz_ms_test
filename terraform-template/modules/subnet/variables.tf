9abb548dc7bae220d2332183b27977c734b5ffc3
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:43:33 -0400

    Update variables.tf

--- terraform-template/modules/subnet/variables.tf
+++ terraform-template/modules/subnet/variables.tf
@@ -1,6 +1,26 @@
-variable "vnet_id" {}
-variable "subnet_name" {}
-variable "address_prefixes" {
-  type = list(string)
+variable "vnet_id" {
+  type        = string
+  description = "The ID of the virtual network"
 }
 
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name"
+}
+
+variable "address_prefixes" {
+  type        = list(string)
+  description = "List of address prefixes for the subnet"
+}
+
+variable "route_table_id" {
+  type        = string
+  description = "ID of the route table to associate (optional)"
+  default     = null
+}
+
+variable "nsg_id" {
+  type        = string
+  description = "ID of the NSG to associate (optional)"
+  default     = null
+}
