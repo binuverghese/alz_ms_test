492f14faa6c53b90998ff94cdacca31712d54cb6
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 21:38:28 -0400

    Create variables.tf

---
+++ firewall_bastion_hub/modules/firewall/variables.tf
@@ -0,0 +1,19 @@
+variable "firewall_ip_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "firewall_policy_name" {
+  type = string
+}
+
+variable "firewall_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
