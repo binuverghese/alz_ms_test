312c4fe77db96be7d6f6f03146eacecebd38d9f4
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 09:12:13 -0400

    Update variables.tf

--- terraform-template/variables.tf
+++ terraform-template/variables.tf
@@ -20,3 +20,18 @@ variable "routes" {
     next_hop_ip    = string
   }))
 }
+variable "security_rules" {
+  description = "List of security rules for the NSG"
+  type = list(object({
+    name                       = string
+    priority                  = number
+    direction                 = string
+    access                    = string
+    protocol                  = string
+    source_port_range         = string
+    destination_port_range    = string
+    source_address_prefix     = string
+    destination_address_prefix = string
+  }))
+  default = []
+}
