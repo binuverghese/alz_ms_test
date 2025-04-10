490c611a5aeee0db944d1e6740d9275e1b09a0ff
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:24:54 -0400

    Update main.tf

--- terraform-template/modules/subnet/main.tf
+++ terraform-template/modules/subnet/main.tf
@@ -1,1 +1,7 @@
+resource "azurerm_subnet" "this" {
+  name                 = var.subnet_name
+  resource_group_name  = split("/", var.vnet_id)[4]
+  virtual_network_name = split("/", var.vnet_id)[8]
+  address_prefixes     = var.address_prefixes
+}
 
