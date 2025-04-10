82d24c4b91b0149a90fa8e04d516ca123374eaef
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:24:18 -0400

    Update variables.tf

--- terraform-template/modules/vnet/variables.tf
+++ terraform-template/modules/vnet/variables.tf
@@ -1,1 +1,13 @@
+variable "vnet_name" {}
+variable "address_space" {
+  type = list(string)
+}
+variable "location" {}
+variable "resource_group_name" {}
+variable "dns_servers" {
+  type = list(string)
+}
+variable "enable_vm_protection" {
+  type = bool
+}
 
