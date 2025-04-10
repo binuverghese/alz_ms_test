63b26fa065ed81b8daae23d020a7b7813ac3cc94
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 22:28:39 -0400

    Update main.tf

--- firewall_bastion_hub/modules/firewall/main.tf
+++ firewall_bastion_hub/modules/firewall/main.tf
@@ -1,15 +1,27 @@
-resource "azurerm_public_ip" "firewall_pip" {
-  name                = var.firewall_ip_name
-  resource_group_name = var.resource_group_name
+resource "azurerm_firewall" "this" {
+  name                = var.name
   location            = var.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  zones               = [1, 2, 3]
+  resource_group_name = var.resource_group_name
+
+  sku_name = var.firewall_sku_name
+  sku_tier = var.firewall_sku_tier
+
+  firewall_policy_id = var.firewall_policy_id
+
+  dynamic "ip_configuration" {
+    for_each = var.firewall_ip_configuration
+    content {
+      name                 = ip_configuration.value.name
+      public_ip_address_id = ip_configuration.value.public_ip_address_id
+      subnet_id            = ip_configuration.value.subnet_id
+    }
+  }
 }
 
-resource "azurerm_firewall_policy" "firewall_policy" {
-  name                = var.firewall_policy_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+output "firewall_private_ip" {
+  value = azurerm_firewall.this.ip_configuration[0].private_ip_address
 }
 
+output "firewall_id" {
+  value = azurerm_firewall.this.id
+}
