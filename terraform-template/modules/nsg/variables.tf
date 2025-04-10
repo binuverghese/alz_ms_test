6efdcd5a2b3f82e01c19019383eb514e890df19f
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 09:00:22 -0400

    Update variables.tf

--- terraform-template/modules/nsg/variables.tf
+++ terraform-template/modules/nsg/variables.tf
@@ -14,18 +14,18 @@ variable "nsg_name" {
 }
 
 variable "security_rules" {
-  description = "List of security rules"
-  type = list(object({
+  description = "Security rules for the Network Security Group"
+  type        = list(object({
     name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_address_prefix      = string
-    source_port_range          = string
+    priority                  = number
+    direction                 = string
+    access                    = string
+    protocol                  = string
+    source_port_range         = string
+    destination_port_range    = string
+    source_address_prefix     = string
     destination_address_prefix = string
-    destination_port_range     = string
   }))
-    #description = "List of security rules"
+  default = []
 }
 
