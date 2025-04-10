e8628ce3b31f4cf2bca0237235354ae2647c200d
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:19:30 -0400

    Update versions.tf

--- terraform-template/versions.tf
+++ terraform-template/versions.tf
@@ -1,1 +1,21 @@
-#versions.tf
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.74"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
