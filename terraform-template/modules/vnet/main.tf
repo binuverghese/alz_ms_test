ef563a197702cbd8f88af181d28003a70938b3cf
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:24:07 -0400

    Update main.tf

--- terraform-template/modules/vnet/main.tf
+++ terraform-template/modules/vnet/main.tf
@@ -1,1 +1,9 @@
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_servers         = var.dns_servers
+  enable_vm_protection = var.enable_vm_protection
+}
 
