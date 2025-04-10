dce2a58dbdffb0488e16844b1cada3646b130269
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:26:05 -0400

    Update variables.tf

--- terraform-template/modules/route_table/variables.tf
+++ terraform-template/modules/route_table/variables.tf
@@ -1,1 +1,12 @@
-#variables.tf
+variable "route_table_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "routes" {
+  type = list(object({
+    name           = string
+    address_prefix = string
+    next_hop_ip    = string
+  }))
+}
+variable "subnet_id" {}
+
