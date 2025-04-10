77eb16907b79e797fc637c4773c43b59106fc621
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 17:59:08 -0400

    Create outputs.tf

---
+++ online/outputs.tf
@@ -0,0 +1,15 @@
+output "vnet_hub_id" {
+  value = module.vnet_hub.id
+}
+
+output "vnet_other_id" {
+  value = module.vnet_other.id
+}
+
+output "dns_resolver_id" {
+  value = module.dns_resolver.id
+}
+
+output "app_gateway_id" {
+  value = module.app_gateway.id
+}
