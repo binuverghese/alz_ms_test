3d2bc576d7740054f0da9414c66d79b56ef67381
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 22:28:17 -0400

    Update variables.tf

--- firewall_bastion_hub/modules/firewall/variables.tf
+++ firewall_bastion_hub/modules/firewall/variables.tf
@@ -1,19 +1,38 @@
-variable "firewall_ip_name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "firewall_policy_name" {
-  type = string
-}
-
-variable "firewall_name" {
-  type = string
+variable "name" {
+  description = "The name of the firewall"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "The location of the firewall"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "firewall_sku_tier" {
+  description = "The SKU tier of the firewall"
+  type        = string
+}
+
+variable "firewall_sku_name" {
+  description = "The SKU name of the firewall"
+  type        = string
+}
+
+variable "firewall_policy_id" {
+  description = "The ID of the firewall policy"
+  type        = string
+}
+
+variable "firewall_ip_configuration" {
+  description = "The list of IP configurations for the firewall"
+  type = list(object({
+    name                 = string
+    public_ip_address_id = string
+    subnet_id            = string
+  }))
 }
