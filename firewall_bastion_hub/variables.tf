cd83f0015db86bdb188ed1b37dc150d302b20ec2
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 21:39:40 -0400

    Update variables.tf

--- firewall_bastion_hub/variables.tf
+++ firewall_bastion_hub/variables.tf
@@ -1,89 +1,33 @@
-# Provider Configuration
-variable "subscription_id" {
-  description = "The Subscription ID for Azure"
-  type        = string
-}
-
-# Resource Group
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-  default     = "rg-dev-003"
-}
+# variables.tf
 
 variable "location" {
-  description = "The Azure region where the resources will be created"
-  type        = string
-  default     = "Canada Central"
+  type    = string
+  default = "Canada Central"
 }
 
-# Firewall Resources
-variable "firewall_pip_name" {
-  description = "The name of the firewall public IP"
-  type        = string
-  default     = "firewall-public-ip"
+variable "resource_group_name" {
+  type    = string
+  default = "rg-dev-001"
 }
 
-variable "firewall_policy_name" {
-  description = "The name of the firewall policy"
-  type        = string
-  default     = "firewall-policy"
-}
-
-variable "firewall_name" {
-  description = "The name of the firewall"
-  type        = string
-  default     = "firewall"
-}
-
-variable "firewall_sku_tier" {
-  description = "The SKU tier for the firewall"
-  type        = string
-  default     = "Standard"
-}
-
-variable "firewall_sku_name" {
-  description = "The SKU name for the firewall"
-  type        = string
-  default     = "AZFW_VNet"
-}
-
-variable "firewall_ipconfig_name" {
-  description = "The name of the firewall IP configuration"
-  type        = string
-  default     = "firewall-ipconfig"
-}
-
-# Bastion Resources
-variable "bastion_ip_name" {
-  description = "The name of the bastion public IP"
-  type        = string
-  default     = "bastion-public-ip"
-}
-
-variable "bastion_host_name" {
-  description = "The name of the bastion host"
-  type        = string
-  default     = "bastion-host"
-}
-
-# DNS VNet Resources
-variable "dns_vnet_name" {
-  description = "The name of the DNS VNet"
-  type        = string
-  default     = "dns-vnet"
-}
-
-# Hub VNet Resources
 variable "hub_vnet_name" {
-  description = "The name of the Hub VNet"
-  type        = string
-  default     = "hub-vnet"
+  type    = string
+  default = "hub-vnet"
 }
 
-# Bastion VNet Resources
-variable "bastion_vnet_name" {
-  description = "The name of the Bastion VNet"
-  type        = string
-  default     = "bastion-vnet"
+variable "hub_vnet_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
 }
+
+variable "hub_vnet_subnets" {
+  type = map(any)
+  default = {
+    AzureFirewallSubnet = {
+      name             = "AzureFirewallSubnet"
+      address_prefixes = ["10.0.0.0/24"]
+    }
+  }
+}
+
+# Define other variables here...
