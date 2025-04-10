ba2fa1a11a491653952ebffa4ce9e3cf29892e28
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 14:40:27 -0400

    Update backend.tf

--- terraform-template/backend.tf
+++ terraform-template/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "rg-dev-001"
     storage_account_name  = "tfstatedemonew"
     container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    key                   = "tfestate1.tfstate"
   }
 }
