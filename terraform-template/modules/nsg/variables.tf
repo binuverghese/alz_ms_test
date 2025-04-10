2f444cccac9db7ed4305a8747024a7c494549c63
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-10 07:39:41 -0400

    Update variables.tf

--- terraform-template/modules/nsg/variables.tf
+++ terraform-template/modules/nsg/variables.tf
@@ -1,1 +1,30 @@
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "nsg_name" {
+  type        = string
+  description = "Network Security Group name"
+}
+
+variable "security_rules" {
+  description = "List of security rules"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_address_prefix      = string
+    source_port_range          = string
+    destination_address_prefix = string
+    destination_port_range     = string
+  }))
+}
 
