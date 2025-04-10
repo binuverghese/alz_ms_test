b86aaef234abb87cf3fc039ed0de56888d66ce79
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-09 21:45:01 -0400

    Update variables.tf

--- firewall_bastion_hub/variables.tf
+++ firewall_bastion_hub/variables.tf
@@ -1,33 +1,94 @@
-# variables.tf
-
-variable "location" {
-  type    = string
-  default = "Canada Central"
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
 }
 
-variable "resource_group_name" {
-  type    = string
-  default = "rg-dev-001"
+variable "location" {
+  description = "The Azure region"
+  type        = string
+}
+
+# Firewall Resources
+variable "firewall_pip_name" {
+  description = "The name for the firewall public IP"
+  type        = string
+}
+
+variable "firewall_policy_name" {
+  description = "The name for the firewall policy"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "The name of the Azure Firewall"
+  type        = string
+}
+
+variable "firewall_ip" {
+  description = "The IP address for the firewall"
+  type        = string
+}
+
+# Bastion Resources
+variable "bastion_ip_name" {
+  description = "The name for the Bastion public IP"
+  type        = string
+}
+
+variable "bastion_host_name" {
+  description = "The name of the Bastion host"
+  type        = string
+}
+
+# DNS Resolver Resources
+variable "dns_resolver_name" {
+  description = "The name of the DNS resolver"
+  type        = string
+}
+
+# VNet Resources
+variable "dns_vnet_name" {
+  description = "The name of the DNS VNet"
+  type        = string
 }
 
 variable "hub_vnet_name" {
-  type    = string
-  default = "hub-vnet"
+  description = "The name of the Hub VNet"
+  type        = string
 }
 
+variable "bastion_vnet_name" {
+  description = "The name of the Bastion VNet"
+  type        = string
+}
+
+# Address Space and Subnets for VNet
 variable "hub_vnet_address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
+  description = "The address space for the Hub VNet"
+  type        = list(string)
+}
+
+variable "dns_vnet_address_space" {
+  description = "The address space for the DNS VNet"
+  type        = list(string)
+}
+
+variable "bastion_vnet_address_space" {
+  description = "The address space for the Bastion VNet"
+  type        = list(string)
 }
 
 variable "hub_vnet_subnets" {
-  type = map(any)
-  default = {
-    AzureFirewallSubnet = {
-      name             = "AzureFirewallSubnet"
-      address_prefixes = ["10.0.0.0/24"]
-    }
-  }
+  description = "The subnets for the Hub VNet"
+  type        = map(any)
 }
 
-# Define other variables here...
+variable "dns_vnet_subnets" {
+  description = "The subnets for the DNS VNet"
+  type        = map(any)
+}
+
+variable "bastion_vnet_subnets" {
+  description = "The subnets for the Bastion VNet"
+  type        = map(any)
+}
