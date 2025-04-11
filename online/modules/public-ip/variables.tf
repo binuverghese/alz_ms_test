e02e3ccee6b0cf7014549d5ef46dff1ede2d9e7b
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:36:48 -0400

    Update variables.tf

--- online/modules/public-ip/variables.tf
+++ online/modules/public-ip/variables.tf
@@ -1,12 +1,14 @@
 variable "name" {
-  type = string
+  description = "The name of the public IP"
+  type        = string
 }
 
 variable "resource_group_name" {
-  type = string
+  description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "The Azure region for the public IP"
+  type        = string
 }
-
