dfc8d0c230ceb50595951df09524aa4c5fb41572
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:38:34 -0400

    Create main.tf

---
+++ online/modules/vnet/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network" "example" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "example" {
+  for_each            = { for subnet in var.subnets : subnet.name => subnet }
+
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes       = [each.value.address_prefix]
+}
