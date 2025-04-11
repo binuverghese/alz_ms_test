747715c5f7460a988197317e252500d4c531e9c2
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:54:10 -0400

    Update main.tf

--- online/modules/dns-resolver/main.tf
+++ online/modules/dns-resolver/main.tf
@@ -1,14 +1,28 @@
-resource "azurerm_firewall" "example" {
-  name                = var.firewall_name
-  location            = var.location
+esource "azurerm_private_dns_resolver" "dns_resolver" {
+  name                = "example-dns-resolver"
   resource_group_name = var.resource_group_name
-  firewall_policy_id  = var.firewall_policy_id
-  sku_name = "AZFW_Hub" 
-  sku_tier = "Standard"  
-  # Public IP configuration for the firewall
-  ip_configuration {
-    name                 = "example-firewall-ipconfig"
-    public_ip_address_id = var.public_ip_id
-    subnet_id            = var.subnet_id
+  location            = var.location
+  virtual_network_id  = var.virtual_network_id
+}
+
+resource "azurerm_private_dns_resolver_inbound_endpoint" "inbound" {
+  name                     = var.inbound_name
+  location                 = var.location
+  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
+
+  ip_configurations {
+    subnet_id = var.inbound_subnet_id
   }
 }
+
+resource "azurerm_private_dns_resolver_outbound_endpoint" "outbound" {
+  name                     = var.outbound_name
+  location                 = var.location
+  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
+
+  subnet_id = var.outbound_subnet_id
+}
+
+
+
+
