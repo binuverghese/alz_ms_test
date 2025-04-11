668e9e54c4a137444b457680a34e5648893f8432
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:31:00 -0400

    Update outputs.tf

--- online/outputs.tf
+++ online/outputs.tf
@@ -6,9 +6,7 @@ output "vnet_other_id" {
   value = module.vnet_other.id
 }
 
-output "dns_resolver_id" {
-  value = module.dns_resolver.id
-}
+
 
 output "app_gateway_id" {
   value = module.app_gateway.id
