7eff59ba5217433f72456abc09b40e2e08988ca9
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:34:28 -0400

    Update main.tf

--- online/modules/dns-resolver/main.tf
+++ online/modules/dns-resolver/main.tf
@@ -1,48 +1,14 @@
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "virtual_network_id" {
-  type = string
-}
-
-variable "inbound_subnet_id" {
-  type = string
-}
-
-variable "outbound_subnet_id" {
-  type = string
-}
-
-resource "azurerm_private_dns_resolver" "dns_resolver" {
-  name                = "example-dns-resolver"
-  resource_group_name = var.resource_group_name
+resource "azurerm_firewall" "example" {
+  name                = var.firewall_name
   location            = var.location
-  virtual_network_id  = var.virtual_network_id
-}
-
-resource "azurerm_private_dns_resolver_inbound_endpoint" "inbound" {
-  name                     = var.inbound_name
-  location                 = var.location
-  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
-
-  ip_configurations {
-    subnet_id = var.inbound_subnet_id
+  resource_group_name = var.resource_group_name
+  firewall_policy_id  = var.firewall_policy_id
+  sku_name = "AZFW_Hub" 
+  sku_tier = "Standard"  
+  # Public IP configuration for the firewall
+  ip_configuration {
+    name                 = "example-firewall-ipconfig"
+    public_ip_address_id = var.public_ip_id
+    subnet_id            = var.subnet_id
   }
 }
-
-resource "azurerm_private_dns_resolver_outbound_endpoint" "outbound" {
-  name                     = var.outbound_name
-  location                 = var.location
-  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
-
-  subnet_id = var.outbound_subnet_id
-}
-
-
-
-
