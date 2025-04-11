1c4f1080d8f93a2e330b0171be576ce1c76f035a
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:31:38 -0400

    Update variables.tf

--- online/variables.tf
+++ online/variables.tf
@@ -21,3 +21,44 @@ variable "firewall_pip_name" {}
 
 variable "appgw_name" {}
 variable "appgw_public_ip_name" {}
+variable "tags" {
+  type        = map(string)
+  description = "Tags to assign to the resources"
+  default     = {
+    environment = "dev"
+    project     = "project_name"
+  }
+}
+# modules/firewall-policy/variables.tf
+variable "firewall_policy_name" {
+  description = "The name of the firewall policy"
+  type        = string
+}
+# modules/firewall-policy/variables.tf
+variable "name" {}
+variable "resource_group_name" {}
+variable "policy_type" {}
+variable "public_ip_id" {
+  description = "The ID of the public IP to associate with the Application Gateway"
+  type        = string
+}
+
+variable "public_ip_name" {
+  description = "The name of the public IP address"
+  type        = string
+}
+
+variable "inbound_name" {
+  description = "Name of the inbound endpoint for DNS resolver"
+  type        = string
+}
+
+variable "outbound_name" {
+  description = "Name of the inbound endpoint for DNS resolver"
+  type        = string
+}
+
+variable "dns_resolver_ip" {
+  description = "The IP address for the DNS resolver"
+  type        = string
+}
