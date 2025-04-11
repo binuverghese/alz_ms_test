c79cd0e29700e40d487638d33e38050b02c0cedd
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:34:08 -0400

    Create variables.tf

---
+++ online/modules/dns-resolver/variables.tf
@@ -0,0 +1,23 @@
+variable "inbound_name" {
+  description = "The name for the inbound DNS resolver endpoint"
+  type        = string
+}
+
+variable "outbound_name" {
+  description = "The name for the outbound DNS resolver endpoint"
+  type        = string
+}
+
+variable "dns_resolver_ip" {
+  description = "The IP address of the DNS resolver"
+  type        = string
+}
+
+
+
+
+
+
+
+
+
