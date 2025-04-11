752e0e63668659a6da107fa319113be52ac8b178
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:33:36 -0400

    Create main.tf

---
+++ online/modules/dns-resolver/main.tf
@@ -0,0 +1,48 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "virtual_network_id" {
+  type = string
+}
+
+variable "inbound_subnet_id" {
+  type = string
+}
+
+variable "outbound_subnet_id" {
+  type = string
+}
+
+resource "azurerm_private_dns_resolver" "dns_resolver" {
+  name                = "example-dns-resolver"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  virtual_network_id  = var.virtual_network_id
+}
+
+resource "azurerm_private_dns_resolver_inbound_endpoint" "inbound" {
+  name                     = var.inbound_name
+  location                 = var.location
+  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
+
+  ip_configurations {
+    subnet_id = var.inbound_subnet_id
+  }
+}
+
+resource "azurerm_private_dns_resolver_outbound_endpoint" "outbound" {
+  name                     = var.outbound_name
+  location                 = var.location
+  private_dns_resolver_id  = azurerm_private_dns_resolver.dns_resolver.id
+
+  subnet_id = var.outbound_subnet_id
+}
+
+
+
+
