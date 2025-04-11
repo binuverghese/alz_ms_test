be98f056063c9cd9b4c9539f9e13a7402c30ea1d
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-11 08:52:25 -0400

    Update variables.tf

--- online/modules/dns-resolver/variables.tf
+++ online/modules/dns-resolver/variables.tf
@@ -14,6 +14,30 @@ variable "dns_resolver_ip" {
 }
 
 
+variable "location" {
+  description = "Location of the DNS Resolver"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name for the DNS Resolver"
+  type        = string
+}
+
+variable "virtual_network_id" {
+  description = "ID of the virtual network"
+  type        = string
+}
+
+variable "inbound_subnet_id" {
+  description = "Subnet ID for inbound endpoint"
+  type        = string
+}
+
+variable "outbound_subnet_id" {
+  description = "Subnet ID for outbound endpoint"
+  type        = string
+}
 
 
 
