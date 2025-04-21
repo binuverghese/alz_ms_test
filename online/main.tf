9c8b65edfcedc69adaa404fad1641dd7c47a2048
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-21 11:41:15 -0400

    Update main.tf

--- online/main.tf
+++ online/main.tf
@@ -1,190 +1,213 @@
-terraform {
-  required_version = ">= 1.3.0, < 2.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0, < 5.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
+module "rg_main" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "0.2.1"
+
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "nsg" {
+  source  = "Azure/avm-res-network-networksecuritygroup/azurerm"
+  version = "~> 0.1"
+
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = module.rg_main.name
+  
+  security_rules = {
+    for idx, rule in var.security_rules : rule.name => {
+      name                       = rule.name
+      priority                   = rule.priority
+      direction                  = rule.direction
+      access                     = rule.access
+      protocol                   = rule.protocol
+      source_port_range          = rule.source_port_range
+      destination_port_range     = rule.destination_port_range
+      source_address_prefix      = rule.source_address_prefix
+      destination_address_prefix = rule.destination_address_prefix
     }
   }
+
+  depends_on = [module.rg_main]
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "1e437fdf-bd78-431d-ba95-1498f0e84c10"
-}
+# Virtual network without subnets
+module "vnet" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.1"
 
-# Resource Group Modules
-module "resource_group_main" {
-  source  = "./modules/resource-group"
-  name    = var.main_rg_name
-  location = var.location
-}
-
-module "resource_group_appgw" {
-  source  = "./modules/resource-group"
-  name    = var.appgw_rg_name
-  location = var.location
-}
-
-module "resource_group_firewall_policy" {
-  source  = "./modules/resource-group"
-  name    = var.firewall_policy_rg_name
-  location = var.location
-}
-
-# VNet Modules
-module "vnet_hub" {
-  source              = "./modules/vnet"
-  name                = var.hub_vnet_name
-  address_space       = var.hub_vnet_address_space
+  name                = var.vnet_name
   location            = var.location
-  resource_group_name = module.resource_group_main.name
+  resource_group_name = module.rg_main.name
+  address_space       = var.address_space_vnet1
   
-  subnets = [
-    {
-      name           = "AzureFirewallSubnet"
-      address_prefix = "10.0.0.0/24"
-    },
-    {
-      name           = "AppGatewaySubnet"
-      address_prefix = "10.0.3.0/24"
-    },
-    {
-      name           = "DNSResolverInbound"
-      address_prefix = "10.0.4.0/28"
-    },
-    {
-      name           = "DNSResolverOutbound"
-      address_prefix = "10.0.5.0/28"
-    }
-  ]
+  enable_vm_protection = var.enable_vm_protection
+  
+  encryption = {
+    enabled     = var.encryption
+    enforcement = var.encryption_enforcement
+    type        = var.encryption_type
+  }
+  
+  flow_timeout_in_minutes = var.flow_timeout_in_minutes
+  
+  # Empty subnets - we'll create separately
+  subnets = {}
 }
 
-module "vnet_other" {
-  source              = "./modules/vnet"
-  name                = var.other_vnet_name
+# Create subnet separately
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = module.rg_main.name
+  virtual_network_name = module.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+  
+  #private_link_endpoint_network_policies_enabled = true
+}
+
+# Explicit subnet-NSG association
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = module.nsg.resource.id
+  
+  depends_on = [azurerm_subnet.main, module.nsg]
+}
+
+# Create resource group for Application Gateway
+module "rg_appgw" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "0.2.1"
+
+  name     = "${var.resource_group_name}-appgw"
+  location = var.location
+}
+
+# Create public IP for Application Gateway
+resource "azurerm_public_ip" "appgw_pip" {
+  name                = "pip-appgw"
+  resource_group_name = module.rg_appgw.name
   location            = var.location
-  resource_group_name = module.resource_group_main.name
-  address_space       = ["10.20.0.0/16"]
-
-  subnets = [
-    {
-      name           = "default"
-      address_prefix = "10.20.1.0/24"
-    },
-    {
-      name           = "app"
-      address_prefix = "10.20.2.0/24"
-    }
-  ]
-}
-
-# VNet Peering Modules
-module "hub_to_other_peering" {
-  source              = "./modules/vnet-peering"
-  name                = "hub-to-other"
-  resource_group_name = module.vnet_hub.resource_group_name
-  vnet_name           = module.vnet_hub.name
-  remote_vnet_id      = module.vnet_other.id
-}
-
-module "other_to_hub_peering" {
-  source              = "./modules/vnet-peering"
-  name                = "other-to-hub"
-  resource_group_name = module.vnet_other.resource_group_name
-  vnet_name           = module.vnet_other.name
-  remote_vnet_id      = module.vnet_hub.id
-}
-
-# DNS Resolver Module
-
-module "dns-resolver" {
-source                      = "./modules/dns-resolver"
-location                    = var.location
-resource_group_name         = module.vnet_hub.resource_group_name
-inbound_name                = var.inbound_name
-outbound_name               = var.outbound_name
-dns_resolver_ip             = var.dns_resolver_ip
-inbound_subnet_id           = module.vnet_hub.subnet_ids["DNSResolverInbound"]
-outbound_subnet_id          = module.vnet_hub.subnet_ids["DNSResolverOutbound"]
-virtual_network_id          = module.vnet_hub.id
-}
-
-
-
-
-# Firewall Policy Module
-module "firewall_policy" {
-  source              = "./modules/firewall-policy"
-  name                = var.firewall_policy_name
-  resource_group_name = module.resource_group_firewall_policy.name
-  location            = var.location
-  policy_type         = "Standard"
-}
-
-# Firewall Module
-module "firewall" {
-  source              = "./modules/firewall"
-  name                = "dev-firewall"
-  location            = var.location
-  resource_group_name = module.resource_group_main.name
-  firewall_name       = "my-firewall"
-  firewall_sku_name   = "AZFW_VNet"
-  firewall_sku_tier   = "Standard"
-  firewall_policy_id  = module.firewall_policy.id
-
-  subnet_id           = module.vnet_hub.subnet_ids["AzureFirewallSubnet"]
-  public_ip_id        = module.firewall_pip.id
-}
-
-# Public IP Module for Firewall
-module "firewall_pip" {
-  source              = "./modules/public-ip"
-  name                = var.firewall_pip_name
-  resource_group_name = module.resource_group_main.name
-  location            = var.location
-}
-
-# Application Gateway Public IP Resource
-resource "azurerm_public_ip" "appgw_public_ip" {
-  name                = "appgw-pip"
-  location            = var.location
-  resource_group_name = module.resource_group_appgw.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  zones               = ["1", "2", "3"]  # Making it zone-redundant to match the Application Gateway zones
 }
 
-# Application Gateway Module
-module "app_gateway" {
-  source                  = "./modules/app-gateway"
-  name                    = var.appgw_name
-  location                = var.location
-  resource_group_name     = module.resource_group_appgw.name
-  app_gateway_name        = "my-app-gateway"
-  sku_name                = "Standard_v2"
-  sku_tier                = "Standard_v2"
-  sku_capacity            = 2
-
-  gateway_ip_config_name  = "appGatewayIpConfig"
-  frontend_ip_config_name = "frontendIpConfig"
-
-  public_ip_address_id    = module.appgw_pip.id
-
-  frontend_port_name      = "port80"
-  frontend_port           = 80
-
-  subnet_id               = module.vnet_hub.subnet_ids["AppGatewaySubnet"]
-  public_ip_id            = azurerm_public_ip.appgw_public_ip.id
+# Create subnet for Application Gateway
+resource "azurerm_subnet" "appgw_subnet" {
+  name                 = "snet-appgw"
+  resource_group_name  = module.rg_main.name
+  virtual_network_name = module.vnet.name
+  address_prefixes     = ["10.1.0.16/28"] # Using a subnet within the VNET's 10.1.0.0/24 range
 }
 
-# Public IP Module for Application Gateway
-module "appgw_pip" {
-  source              = "./modules/public-ip"
-  name                = var.appgw_public_ip_name
-  resource_group_name = module.resource_group_appgw.name
+module "application_gateway" {
+  source             = "Azure/avm-res-network-applicationgateway/azurerm"
+  version            = "0.3.0"
+
+  # Resource group and location (use existing modules)
+  resource_group_name = module.rg_appgw.name
   location            = var.location
+  
+  # Use existing public IP that you already created
+  public_ip_resource_id = azurerm_public_ip.appgw_pip.id
+  create_public_ip      = false
+
+  # Provide Application gateway name 
+  name = var.app_gateway_name
+
+  # Frontend IP configuration names
+  frontend_ip_configuration_public_name = "public-ip-config"
+
+  frontend_ip_configuration_private = {
+    name                          = "private-ip-config"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.1.0.20" # Using an IP from the AppGW subnet CIDR range
+    subnet_id                     = azurerm_subnet.appgw_subnet.id
+  }
+
+  # Gateway IP configuration
+  gateway_ip_configuration = {
+    name      = "appGatewayIpConfig"
+    subnet_id = azurerm_subnet.appgw_subnet.id
+  }
+
+  tags = {
+    environment = "development"
+  }
+
+  # WAF: Using Standard_v2 for better performance and autoscaling capabilities
+  sku = {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+  
+  # Configure availability zones to match the public IP
+  zones = ["1", "2", "3"]
+
+  # Enable autoscaling for better handling of traffic spikes
+  autoscale_configuration = {
+    min_capacity = 2
+    max_capacity = 5
+  }
+
+  # Frontend port configuration
+  frontend_ports = {
+    port_80 = {
+      name = "port_80"
+      port = 80
+    }
+    port_443 = {
+      name = "port_443"
+      port = 443
+    }
+  }
+
+  # Backend address pool configuration
+  backend_address_pools = {
+    default = {
+      name         = "default-backend-pool"
+      ip_addresses = []
+      fqdns        = []
+    }
+  }
+
+  # Backend HTTP settings
+  backend_http_settings = {
+    default = {
+      name                  = "default-http-settings"
+      port                  = 80
+      protocol              = "Http"
+      cookie_based_affinity = "Disabled"
+      path                  = "/"
+      request_timeout       = 30
+    }
+  }
+
+  # HTTP listeners
+  http_listeners = {
+    default = {
+      name                           = "default-listener"
+      frontend_ip_configuration_name = "public-ip-config"
+      frontend_port_name             = "port_80"
+      protocol                       = "Http"
+    }
+  }
+
+  # Request routing rules
+  request_routing_rules = {
+    default = {
+      name                       = "default-rule"
+      rule_type                  = "Basic"
+      http_listener_name         = "default-listener"
+      backend_address_pool_name  = "default-backend-pool"
+      backend_http_settings_name = "default-http-settings"
+      priority                   = 100
+    }
+  }
+
+  depends_on = [
+    module.vnet,
+    azurerm_subnet.appgw_subnet
+  ]
 }
