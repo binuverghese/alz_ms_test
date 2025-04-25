cd2dd5521e635f740cfe4dfebc21ffc938a4ad3a
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-04-25 14:57:41 -0400

    Update variables.tf

--- isolated/variables.tf
+++ isolated/variables.tf
@@ -1,63 +1,153 @@
-variable "location" {
-  type        = string
-  description = "Azure region where resources will be deployed"
+variable "subscription_id" { type = string }
+variable "tenant_id"       { type = string }
+variable "client_id"       { type = string }
+
+variable "location" { type = string }
+
+variable "resource_group_name" { 
+  type = string
+  default = "" # Will be overridden by local.rg_name in main.tf 
 }
 
-variable "resource_group_name" {
+variable "vnet_name" { 
+  type = string
+  default = "" # Will be overridden by local.vnet_name in main.tf 
+}
+
+variable "address_space_vnet1" { type = list(string) }
+variable "dns_servers" {
+
+  description = "List of DNS server IP addresses for the virtual network"
+
+  type        = list(string)
+
+  default     = []
+
+}
+variable "enable_vm_protection" { type = bool }
+
+variable "subnet_name" { 
+  type = string
+  default = "" # Will be overridden by local.subnet_name in main.tf 
+}
+
+variable "subnet_address_prefixes" { type = list(string) }
+
+// Naming components
+variable "business_unit" {
+  description = "Business unit abbreviation (e.g., 'ns')"
   type        = string
-  description = "Name of the resource group"
+  default     = "ns"
+}
+
+variable "region_short" {
+  description = "Short region name (e.g., 'cc' for canadacentral)"
+  type        = string
+  default     = "cc"
+}
+
+variable "archetype" {
+  description = "Architecture type (e.g., 'conn', 'corp')"
+  type        = string
+  default     = "conn"
+}
+
+variable "workload" {
+  description = "Workload name (e.g., 'shs')"
+  type        = string
+  default     = "shs"
+}
+
+variable "environment" {
+  description = "Environment name (e.g., 'dev', 'prod')"
+  type        = string
+  default     = "dev"
+}
+
+variable "workload_description" {
+  description = "Workload description (e.g., 'core_hub')"
+  type        = string
+  default     = "core_hub"
+}
+
+variable "security_rules" {
+  description = "List of security rules for the NSG"
+  type = list(object({
+    name                       = string
+    priority                  = number
+    direction                 = string
+    access                    = string
+    protocol                  = string
+    source_port_range         = string
+    destination_port_range    = string
+    source_address_prefix     = string
+    destination_address_prefix = string
+  }))
+  default = []
+}
+
+variable "create_nsg" {
+  type    = bool
+  default = true  # Set based on your needs
+}
+
+variable "create_route_table" {
+  type    = bool
+  default = true  # Set based on your needs
 }
 
 variable "route_table_name" {
+  description = "The name of the route table"
   type        = string
-  description = "Name of the route table"
+  default     = "" # Will be overridden by local.rt_name in main.tf
 }
 
 variable "nsg_name" {
+  description = "The name of the Network Security Group"
   type        = string
-  description = "Name of the network security group"
+  default     = "" # Will be overridden by local.nsg_name in main.tf
 }
 
-variable "vnet_name" {
+variable "rg_main" {
+  description = "The name of the Resource Group for main"
   type        = string
-  description = "Name of the virtual network"
-}
-
-variable "address_space_vnet1" {
-  type        = list(string)
-  description = "Address space for the virtual network"
-}
-
-variable "dns_servers" {
-  type        = list(string)
-  description = "List of DNS servers"
-}
-
-variable "enable_vm_protection" {
-  type        = bool
-  description = "Enable VM protection"
+  default     = "" # Will be overridden by local.rg_name in main.tf
 }
 
 variable "encryption" {
-  type        = bool
   description = "Enable encryption"
+  type        = bool
 }
 
-variable "subnet_name" {
+variable "flow_timeout_in_minutes" {
+  description = "Flow timeout in minutes"
+  type        = number
+  default     = 4
+}
+
+variable "encryption_enforcement" {
+  type    = string
+  default = "DropUnencrypted"
+}
+
+variable "encryption_type" {
+  type    = string
+  default = "enabled"
+}
+
+variable "remote_virtual_network_id" {
+  description = "The resource ID of the remote virtual network for peering. Set to null to disable peering."
   type        = string
-  description = "Subnet name"
-}
-
-variable "subnet_address_prefixes" {
-  type        = list(string)
-  description = "Subnet address prefixes"
+  default     = null
 }
 
 variable "routes" {
+  description = "List of routes for the route table"
   type = list(object({
     name           = string
     address_prefix = string
+    next_hop_type  = string
     next_hop_ip    = string
   }))
-  description = "List of routes for the route table"
+  default = []
 }
